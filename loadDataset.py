5043f0a6e25d3ee306ccd17a5debcdf6b779ed5f
Jacob Redden <jacob.redden77@gmail.com>
2024-03-30 12:50:57 -0500

    Add main qualifier to loadDataset

--- loadDataset.py
+++ loadDataset.py
@@ -1,9 +1,16 @@
 import datasets
 import pandas as pd
 
-dataset_info = datasets.load_dataset_builder("tpremoli/CelebA-attrs")
 
-print(dataset_info.info.features)
-print(dataset_info.info.splits)
+def load_dataset():
+    dataset_info = datasets.load_dataset_builder("tpremoli/CelebA-attrs")
 
-dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="train")
+    print(dataset_info.info.features)
+    print(dataset_info.info.splits)
+
+    celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="train")
+
+    return celeb_dataset
+
+if __name__ == '__main__':
+    celeb_dataset = load_dataset()
