f25e10eef55ac2aae2d51131565337e2f8165592
Jacob Redden <jacob.redden77@gmail.com>
2024-03-30 12:51:09 -0500

    Add argParser

---
+++ argParser.py
@@ -0,0 +1,53 @@
+import argparse
+import sys
+
+ALL_MODELS = ['dt', 'nb', 'lr', 'svm', 'mlp']
+
+def get_arg_parser():
+
+    parser = argparse.ArgumentParser(description='Parse Command Line Arguments for COMP6600 Project.')
+    parser.add_argument('-m', '--models', nargs='+',
+                        choices=['decision tree', 'dt',
+                                'naive bayes', 'nb',
+                                'logistic regression', 'lr',
+                                'support vector machine', 'svm',
+                                'multi-layered perceptron', 'mlp',
+                                'all'],
+                        default=['all'], required=False, dest='models',
+                        help='which models to run')
+    
+    return parser
+
+def parse_args(args):
+    parser = get_arg_parser()
+    parsed_args = parser.parse_args(args)
+
+    models = {'dt': False,
+              'nb': False,
+              'lr': False,
+              'svm': False,
+              'mlp': False}
+    if 'all' in parsed_args.models:
+        models['dt'] = True
+        models['nb'] = True
+        models['lr'] = True
+        models['svm'] = True
+        models['mlp'] = True
+    if 'dt' in parsed_args.models or 'decision tree' in parsed_args.models:
+        models['dt'] = True
+    if 'nb' in parsed_args.models or 'naive bayes' in parsed_args.models:
+        models['nb'] = True
+    if 'lr' in parsed_args.models or 'logistic regression' in parsed_args.models:
+        models['lr'] = True
+    if 'svm' in parsed_args.models or 'support vector machine' in parsed_args.models:
+        models['svm'] = True
+    if 'mlp' in parsed_args.models or 'multi-layered perceptron' in parsed_args.models:
+        models['svm'] = True
+    
+    return models
+    
+if __name__ == '__main__':
+    models = parse_args(sys.argv[1:])
+
+    print(models)
+
