2b258df8ab25b1bd52b583eb611fe23c389b54a6
Jacob Redden <jacob.redden77@gmail.com>
2024-03-31 14:28:47 -0500

    loadDataset work with pickles

--- loadDataset.py
+++ loadDataset.py
@@ -1,42 +1,114 @@
 import datasets
-import pandas as pd
 import numpy as np
-from PIL import Image
+from tqdm import tqdm
+import pickle
 
 import os
 import shutil
 
 def configure_dataset(celeb_dataset):
-    print(celeb_dataset)
-    print(np.array(celeb_dataset[0]['image']))
+    X = np.zeros((len(celeb_dataset), len(np.array(celeb_dataset[0]['image']).flatten())))
+    y = np.zeros((len(celeb_dataset), 4))
+    for i in tqdm(range(len(celeb_dataset))):
+        X[i] = np.array(celeb_dataset[0]['image']).flatten()
+        if celeb_dataset[i]['Black_Hair'] == 1:
+            y[i][0] = 1
+        if celeb_dataset[i]['Blond_Hair'] == 1:
+            y[i][1] = 1
+        if celeb_dataset[i]['Brown_Hair'] == 1:
+            y[i][2] = 1
+        if celeb_dataset[i]['Gray_Hair'] == 1:
+            y[i][3] = 1
 
-    return celeb_dataset
+    return X, y
 
 def load_training_dataset():
-    print('Loading training dataset... ', end='', flush=True)
-    celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="train")
+    print('Loading training dataset... ')
+    dataset_pickle_filename = 'training_dataset.pickle'
+    target_pickle_filename = 'training_targets.pickle'
+
+    celeb_dataset = None
+    targets = None
+    if os.path.isfile(dataset_pickle_filename) and os.path.isfile(target_pickle_filename):
+
+        with open(dataset_pickle_filename, 'rb') as handle:
+            celeb_dataset = pickle.load(handle)
+        with open(target_pickle_filename, 'rb') as handle:
+            targets = pickle.load(handle)
+    else:
+        celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="train")
+
+        celeb_dataset, targets = configure_dataset(celeb_dataset)
+
+        with open(dataset_pickle_filename, 'wb') as handle:
+            pickle.dump(celeb_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(target_pickle_filename, 'wb') as handle:
+            pickle.dump(targets, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print('Done.')
 
-    celeb_dataset = configure_dataset(celeb_dataset)
-
-    return celeb_dataset
+    return celeb_dataset, targets
 
 def load_testing_dataset():
-    print('Loading testing dataset... ', end='', flush=True)
-    celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="test")
-    print('Done.')
+    print('Loading testing dataset... ')
+    dataset_pickle_filename = 'testing_dataset.pickle'
+    target_pickle_filename = 'testing_targets.pickle'
 
-    return celeb_dataset
+    celeb_dataset = None
+    targets = None
+    if os.path.isfile(dataset_pickle_filename) and os.path.isfile(target_pickle_filename):
+
+        with open(dataset_pickle_filename, 'rb') as handle:
+            celeb_dataset = pickle.load(handle)
+        with open(target_pickle_filename, 'rb') as handle:
+            targets = pickle.load(handle)
+    
+    else:
+        celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="test")
+        celeb_dataset, targets = configure_dataset(celeb_dataset)
+
+        with open(dataset_pickle_filename, 'wb') as handle:
+            pickle.dump(celeb_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(target_pickle_filename, 'wb') as handle:
+            pickle.dump(targets, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print("Done.")
+    return celeb_dataset, targets
 
 def load_validation_dataset():
-    print('Loading validation dataset... ', end='', flush=True)
-    celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="validation")
-    print('Done.')
+    print('Loading validation dataset... ')
+    dataset_pickle_filename = 'validation_dataset.pickle'
+    target_pickle_filename = 'validation_targets.pickle'
 
-    return celeb_dataset
+    celeb_dataset = None
+    targets = None
+    if os.path.isfile(dataset_pickle_filename) and os.path.isfile(target_pickle_filename):
+
+        with open(dataset_pickle_filename, 'rb') as handle:
+            celeb_dataset = pickle.load(handle)
+        with open(target_pickle_filename, 'rb') as handle:
+            targets = pickle.load(handle)
+    
+    else:
+        celeb_dataset = datasets.load_dataset("tpremoli/CelebA-attrs", split="validation")
+        celeb_dataset, targets = configure_dataset(celeb_dataset)
+
+        with open(dataset_pickle_filename, 'wb') as handle:
+            pickle.dump(celeb_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(target_pickle_filename, 'wb') as handle:
+            pickle.dump(targets, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print("Done.")
+    return celeb_dataset, targets
 
 def clear_cache():
     shutil.rmtree(os.path.expanduser('~/.cache/huggingface/'))
+    os.remove('training_dataset.pickle')
+    os.remove('training_targets.pickle')
+    os.remove('testing_dataset.pickle')
+    os.remove('testing_targets.pickle')
+    os.remove('validation_dataset.pickle')
+    os.remove('validation_targets.pickle')
+
 
 if __name__ == '__main__':
     celeb_dataset = load_training_dataset()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from sklearn import tree
 
 import argParser
 import loadDataset
@@ -10,11 +11,11 @@ if clear:
     print("Cleared huggingface cache")
     exit()
 
-celeb_training_dataset = loadDataset.load_training_dataset()
-celeb_testing_dataset = loadDataset.load_testing_dataset()
+celeb_training_dataset, celeb_training_targets = loadDataset.load_training_dataset()
+celeb_testing_dataset, celeb_testing_targets = loadDataset.load_testing_dataset()
 
 # Train Models
-
+sklearn.
 
 
 # Validation of Models
