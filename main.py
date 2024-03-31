d67092363ead58e96117808bb82c6d639b0d2e44
Jacob Redden <jacob.redden77@gmail.com>
2024-03-31 14:41:50 -0500

    Add basic models with default hyper parameters

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sys
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 
 import argParser
 import loadDataset
@@ -15,10 +19,26 @@ celeb_training_dataset, celeb_training_targets = loadDataset.load_training_datas
 celeb_testing_dataset, celeb_testing_targets = loadDataset.load_testing_dataset()
 
 # Train Models
-dt = tree.DecisionTreeClassifier()
-dt.fit(celeb_training_dataset, celeb_training_targets)
-tree.plot_tree(dt)
+if models['dt']:
+    dt = DecisionTreeClassifier()
+    dt.fit(celeb_training_dataset, celeb_training_targets)
+    # tree.plot_tree(dt)
 
+if models['nb']:
+   nb = GaussianNB()
+   nb.fit(celeb_training_dataset, celeb_training_targets) 
+
+if models['lr']:
+    lr = LinearRegression()
+    lr.fit(celeb_training_dataset, celeb_training_targets)
+
+if models['svm']:
+    svm = SVC()
+    svm.fit(celeb_training_dataset, celeb_training_targets)
+
+if models['mlp']:
+    mlp = MLPClassifier()
+    mlp.fit(celeb_training_dataset, celeb_training_targets)
 
 # Validation of Models
 
